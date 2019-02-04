mheyer32/scummvm-amigaos3

commit fada08e2277954fe106f6654ea00eb902cc5ee39

sci: Use an Array instead of HashMap for all object storage
This allows muchfaster access to the objects via just their offset -
instead of going through a hashmap, we directly use the offset
to access an array of object pointers. In particular one of the high-hitting
functions SegManager::getObject() will benefit from that.

This is only enabled, when SCI32 is disabled, since SCI32 allows much
bigger offsets. Not sure if we can use the array approach there, too
without having to create a really huge array. I did not investigate into
the sparseness of the object IDs (offsets).

One wart in this approach is places where we need to traverse the list
of live objects. There can be potentially unused slots, so we have to skip
them when traversing the array. Hide this ugliness behind a macro.

engines/sci/engine/script.cpp
@@ -67,7 +67,17 @@ void Script::freeScript(const bool keepLocalsSegment) {
 
 	_lockers = 1;
 	_markedAsDeleted = false;
+
+#ifdef ENABLE_SCI32
 	_objects.clear();
+#else
+	for (ObjMap::iterator it = _objects.begin(); it != _objects.end(); ++it ){
+		delete *it;
+	}
+	_objects.resize(0);
+	_objects.reserve(0xFFFF);
+	_numObjects = 0;
+#endif
 
 	_offsetLookupArray.clear();
 	_offsetLookupObjectCount = 0;
@@ -663,7 +673,17 @@ Object *Script::scriptObjInit(reg_t obj_pos, bool fullObjectInit) {
 
 	// Get the object at the specified position and init it. This will
 	// automatically "allocate" space for it in the _objects map if necessary.
+#ifndef ENABLE_SCI32
+	const auto offset = obj_pos.getOffset();
+	_objects.resize(MAX(_objects.size(), (size_t)offset +1));
+	if (!_objects[offset]) {
+		_objects[offset] = new Object();
+		++_numObjects;
+	}
+	Object *obj = _objects[offset];
+#else
 	Object *obj = &_objects[obj_pos.getOffset()];
+#endif
 	obj->init(*this, obj_pos, fullObjectInit);
 
 	return obj;
@@ -824,9 +844,11 @@ void Script::relocateSci0Sci21(const SegmentId segmentId) {
 			// Not a local? It's probably an object or code block. If it's an
 			// object, relocate it.
 			const ObjMap::iterator end = _objects.end();
-			for (ObjMap::iterator it = _objects.begin(); it != end; ++it)
-				if (it->_value.relocateSci0Sci21(segmentId, pos, getHeapOffset()))
+			for (ObjMap::iterator it = _objects.begin(); it != end; ++it) {
+				OBJECT_FROM_ITERATOR()
+				if (object.relocateSci0Sci21(segmentId, pos, getHeapOffset()))
 					break;
+			}
 		}
 	}
 }
@@ -1133,7 +1155,15 @@ void Script::initializeObjectsSci0(SegManager *segMan, SegmentId segmentId) {
 								// #3150767.
 								// Same happens with script 764, it seems to
 								// contain junk towards its end.
+#ifdef ENABLE_SCI32
 								_objects.erase(addr.toUint16() - SCRIPT_OBJECT_MAGIC_OFFSET);
+#else
+								// FIXME: have a deleteObject function?
+								const auto offset = addr.toUint16() - SCRIPT_OBJECT_MAGIC_OFFSET;
+								delete _objects[offset];
+								_objects[offset] = NULL;
+								--_numObjects;
+#endif
 							} else {
 								error("Failed to locate base object for object at %04x:%04x in script %d", PRINT_REG(addr), _nr);
 							}
@@ -1306,10 +1336,11 @@ Common::Array<reg_t> Script::listObjectReferences() const {
 		tmp.push_back(make_reg(_localsSegment, 0));
 
 	// All objects (may be classes, may be indirectly reachable)
-	ObjMap::iterator it;
-	const ObjMap::iterator end = _objects.end();
+	ObjMap::const_iterator it;
+	const ObjMap::const_iterator end = _objects.end();
 	for (it = _objects.begin(); it != end; ++it) {
-		tmp.push_back(it->_value.getPos());
+		OBJECT_FROM_ITERATOR();
+		tmp.push_back(object.getPos());
 	}
 
 	return tmp;

engines/sci/console.cpp
@@ -2450,10 +2450,13 @@ bool Console::segmentInfo(int nr) {
 		for (it = objects.begin(); it != end; ++it) {
 			debugPrintf("    ");
 			// Object header
-			const Object *obj = _engine->_gamestate->_segMan->getObject(it->_value.getPos());
+			OBJECT_FROM_ITERATOR();
+			const auto regpos = object.getPos();
+			const Object *obj = _engine->_gamestate->_segMan->getObject(regpos);
+
 			if (obj)
-				debugPrintf("[%04x:%04x] %s : %3d vars, %3d methods\n", PRINT_REG(it->_value.getPos()),
-							_engine->_gamestate->_segMan->getObjectName(it->_value.getPos()),
+				debugPrintf("[%04x:%04x] %s : %3d vars, %3d methods\n", PRINT_REG(regpos),
+							_engine->_gamestate->_segMan->getObjectName(regpos),
 							obj->getVarCount(), obj->getMethodCount());
 		}
 	}
@@ -3692,11 +3695,14 @@ void Console::printKernelCallsFound(int kernelFuncNum, bool showFoundScripts) {
 
 		// Iterate through all the script's objects
 		const ObjMap &objects = script->getObjectMap();
+		//FIXME: replace objectmap traversal with general foreach_object function, using
+		//lambdas. This would get us around the weirdo iterator macro
 		ObjMap::const_iterator it;
 		const ObjMap::const_iterator end = objects.end();
 		for (it = objects.begin(); it != end; ++it) {
-			const Object *obj = customSegMan->getObject(it->_value.getPos());
-			const char *objName = customSegMan->getObjectName(it->_value.getPos());
+			OBJECT_FROM_ITERATOR();
+			const Object *obj = &object;
+			const char *objName = customSegMan->getObjectName(object.getPos());
 
 			// Now dissassemble each method of the script object
 			for (uint16 i = 0; i < obj->getMethodCount(); i++) {

engines/sci/engine/savegame.cpp
@@ -281,7 +281,10 @@ void SegManager::saveLoadWithSerializer(Common::Serializer &s) {
 
 				ObjMap &objects = scr->getObjectMap();
 				for (ObjMap::iterator it = objects.begin(); it != objects.end(); ++it) {
-					reg_t addr = it->_value.getPos();
+
+					OBJECT_FROM_ITERATOR();
+					auto addr = object.getPos();
+
 					if (pass == 1) {
 						scr->scriptObjInit(addr, false);
 					} else {
@@ -570,21 +573,36 @@ void Script::saveLoadWithSerializer(Common::Serializer &s) {
 	// By "chance" this format is identical to the format used to sync Common::Array<>,
 	// hence we can still old savegames with identical code :).
 
+#ifdef ENABLE_SCI32
 	uint numObjs = _objects.size();
+#else
+	uint numObjs = _numObjects;
+#endif
 	s.syncAsUint32LE(numObjs);
 
 	if (s.isLoading()) {
-		_objects.clear();
+#ifdef ENABLE_SCI32
+		assert(_objects.empty());
+#endif
 		Object tmp;
 		for (uint i = 0; i < numObjs; ++i) {
 			syncWithSerializer(s, tmp);
+#ifdef ENABLE_SCI32
 			_objects[tmp.getPos().getOffset()] = tmp;
+#else
+			const auto offset = tmp.getPos().getOffset();
+			_objects.resize(MAX(_objects.size(), (size_t)offset + 1));
+			assert(_objects[offset] == NULL);
+			_objects[offset] = new Object();
+			*_objects[offset] = tmp;
+#endif
 		}
 	} else {
 		ObjMap::iterator it;
 		const ObjMap::iterator end = _objects.end();
 		for (it = _objects.begin(); it != end; ++it) {
-			syncWithSerializer(s, it->_value);
+			OBJECT_FROM_ITERATOR()
+			syncWithSerializer(s, object);
 		}
 	}
 

engines/sci/engine/script.h
@@ -54,7 +54,22 @@ enum ScriptObjectTypes : byte {
 	SCI_OBJ_LOCALVARS
 };
 
+#ifdef ENABLE_SCI32
 typedef Common::HashMap<uint32, Object> ObjMap;
+#define OBJECT_FROM_ITERATOR() auto &object = it->_value;
+#else
+// This is a hack: when switching from HashMap to Array, we now
+// have to deal with 'holes' in the array where there's no object allocated yet.
+// We need to skip these holes as the original HashMap iterator only enumerates
+// active objects.
+typedef Common::Array<Object *> ObjMap;
+
+#define OBJECT_FROM_ITERATOR() \
+	if (!(*it)) {              \
+		continue;              \
+	}                          \
+	auto &object = *(*it);
+#endif
 
 enum ScriptOffsetEntryTypes : byte {
 	SCI_SCR_OFFSET_TYPE_OBJECT = 0, // classes are handled by this type as well
@@ -99,7 +114,9 @@ private:
 	LocalVariables *_localsBlock;
 
 	ObjMap _objects;	/**< Table for objects, contains property variables */
-
+#ifndef ENABLE_SCI32
+	Offset _numObjects; /**< number of live objects in 'objects' */
+#endif
 protected:
 	offsetLookupArrayType _offsetLookupArray; // Table of all elements of currently loaded script, that may get pointed to
 
@@ -164,22 +181,38 @@ public:
 
 	virtual void saveLoadWithSerializer(Common::Serializer &ser);
 
-	Object *getObject(uint32 offset) {
+#ifdef ENABLE_SCI32
+	Object *getObject(Offset offset) {
 		assert(_objects.contains(offset));
 		return &_objects[offset];
 	}
 
-	const Object *getObject(uint32 offset) const {
+	const Object *getObject(Offset offset) const {
 		assert(_objects.contains(offset));
 		return &_objects[offset];
 	}
 
-	const Object *findObject(uint32 offset) const {
+	const Object *findObject(Offset offset) const {
 		if (_objects.contains(offset))
 			return &_objects[offset];
 		else
 			return 0;
 	}
+#else
+	Object *getObject(Offset offset) {
+		assert(_objects[offset]);
+		return _objects[offset];
+	}
+
+	const Object *getObject(Offset offset) const {
+		assert(_objects[offset]);
+		return _objects[offset];
+	}
+
+	const Object *findObject(Offset offset) const {
+		return _objects[offset];
+	}
+#endif
 
 	/**
 	 * Initializes an object within the segment manager

engines/sci/engine/seg_manager.cpp
@@ -255,7 +255,7 @@ SegmentObj *SegManager::getSegment(SegmentId seg, SegmentType type) const {
 Object *SegManager::getObject(const reg_t &pos) const {
 	SegmentObj *mobj = findSegmentObj(pos.getSegment());
 	Object *obj = NULL;
-	const uint32 offset = pos.getOffset();
+	const auto offset = pos.getOffset();
 	if (mobj != NULL) {
 		if (mobj->getType() == SEG_TYPE_CLONES) {
 			CloneTable &ct = *(CloneTable *)mobj;
@@ -310,7 +310,8 @@ Common::Array<reg_t> SegManager::findObjectsByName(const Common::String &name) {
 			const Script *scr = (const Script *)mobj;
 			const ObjMap &objects = scr->getObjectMap();
 			for (ObjMap::const_iterator it = objects.begin(); it != objects.end(); ++it) {
-				objpos.setOffset(it->_value.getPos().getOffset());
+				OBJECT_FROM_ITERATOR();
+				objpos.setOffset(object.getPos().getOffset());
 				if (name == getObjectName(objpos))
 					result.push_back(objpos);
 			}

engines/sci/engine/vm_types.cpp
@@ -40,7 +40,7 @@ void reg_t::setSegment(SegmentId segment) {
 	}
 }
 
-void reg_t::setOffset(uint32 offset) {
+void reg_t::setOffset(Offset offset) {
 #ifdef ENABLE_SCI32
 	if (getSciVersion() == SCI_VERSION_3) {
 		// Store the lower 16 bits in the offset, and the 17th and 18th bits in the segment

engines/sci/engine/vm_types.h
@@ -30,6 +30,11 @@ namespace Sci {
 
 // Segment ID type
 typedef uint16 SegmentId;
+#ifdef ENABLE_SCI32
+typedef uint32 Offset;
+#else
+typedef uint16 Offset;
+#endif
 
 enum {
 	kUninitializedSegment = 0x1FFF,
@@ -46,10 +51,10 @@ struct reg_t {
 
 	void setSegment(SegmentId segment);
 
-	uint32 getOffset() const;
-	void setOffset(uint32 offset);
+	Offset getOffset() const;
+	void setOffset(Offset offset);
 
-	inline void incOffset(int32 offset) {
+	inline void incOffset(Offset offset) {
 		setOffset(getOffset() + offset);
 	}
 
@@ -187,7 +192,7 @@ inline SegmentId reg_t::getSegment() const {
 	}
 }
 
-inline uint32 reg_t::getOffset() const {
+inline Offset reg_t::getOffset() const {
 #ifdef ENABLE_SCI32
 	if (getSciVersion() == SCI_VERSION_3) {
 		// Return the lower 16 bits from the offset, and the 17th and 18th bits from the segment

commit 18728d248dbc005f1f526ccd23bb6b222244b6f7

SCI: fix KQ5 crash
This bug was introduced by

commit fada08e2277954fe106f6654ea00eb902cc5ee39
sci: Use an Array instead of HashMap for all object storage

engines/sci/engine/script.cpp
@@ -1155,9 +1155,12 @@ void Script::initializeObjectsSci0(SegManager *segMan, SegmentId segmentId) {
 #else
 								// FIXME: have a deleteObject function?
 								const auto offset = addr.toUint16() - SCRIPT_OBJECT_MAGIC_OFFSET;
-								delete _objects[offset];
-								_objects[offset] = NULL;
-								--_numObjects;
+								debug(1, "initializeObjectsSci0 got KQV offset %d, numObjects %d", offset, _numObjects);
+								if (offset < _numObjects) {
+									delete _objects[offset];
+									_objects[offset] = NULL;
+									--_numObjects;
+								}
 #endif
 							} else {
 								error("Failed to locate base object for object at %04x:%04x in script %d", PRINT_REG(addr), _nr);